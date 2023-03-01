715f6c94952d39a5e8f5891cadd1cec935368076
Raphael Pacheco <raphaelrogen@gmail.com>
2023-03-01 00:52:48 -0300

    [Requisito 02] - Concluido

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,51 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+/* import Example from '../database/models/ExampleModel'; */
+import Team from '../database/models/Team';
+
+import { Response } from 'superagent';
+import teams from './utils/teamMock';
+
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testes do endpont /teams ', () => {
+  
+   let chaiHttpResponse: Response;
+
+   before(async () => {
+     sinon
+       .stub(Team, "findAll")
+       .resolves( teams as Team[]);
+   });
+
+   after(()=>{
+     (Team.findAll as sinon.SinonStub).restore();
+   })
+
+   it('espera a resposta de todos os times', async () => {
+    const response = chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams')
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal(teams);  
+   });
+
+  it('Seu sub-teste', () => {
+    expect(false).to.be.eq(true);
+  });
+});
+function before(arg0: () => Promise<void>) {
+  throw new Error('Function not implemented.');
+}
+
+function after(arg0: () => void) {
+  throw new Error('Function not implemented.');
+}
+
---
+++ app/backend/src/tests/utils/teamMock.ts
@@ -0,0 +1,69 @@
+const teams: Array<object> = [
+  {
+    id:1,
+    team_name: 'Avaí/Kindermann',
+  },
+  {
+    id:2,
+    team_name: 'Bahia',
+  },
+  {
+    id:3,
+    team_name: 'Botafogo',
+  },
+  {
+    id:4,
+    team_name: 'Corinthians',
+  },
+  {
+    id:5,
+    team_name: 'Cruzeiro',
+  },
+  {
+    id:6,
+    team_name: 'Ferroviaria',
+  },
+  {
+    id:7,
+    team_name: 'Flamengo',
+  },
+  {
+    id:8,
+    team_name: 'Grêmio',
+  },
+  {
+    id:9,
+    team_name: 'Internacional',
+  },
+  {
+    id:10,
+    team_name: 'Minas Brasília',
+  },
+  {
+    id:11,
+    team_name: 'Napoli-SC',
+  },
+  {
+    id:12,
+    team_name: 'Palmeiras',
+  },
+  {
+    id:13,
+    team_name: 'Real Brasília',
+  },
+  {
+    id:14,
+    team_name: 'Santos',
+  },
+  {
+    id:15,
+    team_name: 'São José-SP',
+  },
+  {
+    id:16,
+    team_name: 'São Paulo',
+  },
+  
+];
+
+export default teams;
