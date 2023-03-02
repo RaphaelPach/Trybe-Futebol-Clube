d2f73042b8d7aedf251ca5bb3eac6422fd378f31
Raphael Pacheco <raphaelrogen@gmail.com>
2023-03-02 20:43:07 -0300

    [Requisito 10] - Concluido

--- app/backend/src/Utils/validates.ts
+++ app/backend/src/Utils/validates.ts
@@ -3,10 +3,10 @@ import CustomError from '../Error/CustomError';
 
 export const ValidEmail = (email: string) => {
   const { error } = emailSchema.validate(email);
-  if (error) throw new CustomError('Email inválido', '401');
+  if (error) throw new CustomError('Invalid email or password', '401');
 };
 
 export const ValidPass = (password: string) => {
   const { error } = passwordSchema.validate(password);
-  if (error) throw new CustomError('password inválido', '401');
+  if (error) throw new CustomError('Invalid email or password', '401');
 };
